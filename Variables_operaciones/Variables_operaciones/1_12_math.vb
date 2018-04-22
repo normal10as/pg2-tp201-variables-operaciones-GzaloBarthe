770d7776021b476e647662145b246630f512391c
Gzalo Barthe <gzalo1590@hotmail.com>
2018-04-22 05:36:23 -0300

    nombres con nro de ejercicio

---
+++ Variables_operaciones/Variables_operaciones/1_12_math.vb
@@ -0,0 +1,20 @@
+﻿Module _1_12_math
+    Sub main()
+        Dim nro As Single
+        Dim defecto As Single
+        Dim exceso As Single
+        Dim redondeo As Integer
+        Console.WriteLine("ingrese un nro")
+        nro = Console.ReadLine()
+        defecto = Math.Floor(nro)
+        exceso = Math.Ceiling(nro)
+        redondeo = Math.Round(nro)
+        Console.WriteLine("por defecto: " & defecto)
+        Console.WriteLine("por exceso: " & exceso)
+        Console.WriteLine("redondeo: " & redondeo)
+
+    End Sub
+
+
+
+End Module
